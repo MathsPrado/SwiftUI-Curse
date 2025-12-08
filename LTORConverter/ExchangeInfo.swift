224283dacf0f7648ce5dc629e2be644cd7c1b3db
MathsPrado <matheusprado55@gmail.com>
2025-12-07 21:21:22 -0300

    ädded

--- LTORConverter/ExchangeInfo.swift
+++ LTORConverter/ExchangeInfo.swift
@@ -31,21 +31,15 @@ struct ExchangeInfo: View {
                 //description Text
                 
                 //Exchange rates
+                ExchangeRate(leftImage: .goldpiece, RightImage: .goldpiece, messagetext: "1 Gold Piece = 4 Gold Pennies")
+                ExchangeRate(leftImage: .goldpenny, RightImage: .silverpiece, messagetext: "1 Gold Penny = 4 Silver Pieces")
                 
-                HStack{
-                    Image(.goldpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 33)
-                    
-                    Text("1 Gold Piece = 4 = Gold Pennies")
-                    
-                    Image(.goldpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    
-                }
+                ExchangeRate(leftImage: .silverpiece, RightImage: .silverpenny, messagetext: "1 Silver Piece = 4 Gold Pennies")
+                ExchangeRate(leftImage: .silverpenny, RightImage: .copperpenny, messagetext: "1 Silver Penny = 100 Copper Pennies")
+//                ExchangeRate()
+//                ExchangeRate()
+//                ExchangeRate()
+//                ExchangeRate()
                 
                 Button("Done"){
 
---
+++ LTORConverter/ExchangeRate.swift
@@ -0,0 +1,35 @@
+//
+//  ExchangeRate.swift
+//  LTORConverter
+//
+//  Created by Matheus Prado on 06/12/25.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let RightImage: ImageResource
+    let messagetext: String
+    
+    
+    var body: some View {
+        HStack{
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 33)
+            
+            Text(messagetext)
+            
+            Image(RightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+        }    }
+}
+
+#Preview {
+    ExchangeRate(leftImage: .goldpiece, RightImage: .goldpenny, messagetext: "1 Gold Piece = 4 = Gold Pennies")
+}
