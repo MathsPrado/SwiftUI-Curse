0f9eb81ca629c643b30637c9edbae95e58e4f3c8
MathsPrado <matheusprado55@gmail.com>
2025-12-08 22:08:50 -0300

    added call view

--- LTORConverter/ExchangeInfo.swift
+++ LTORConverter/ExchangeInfo.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ExchangeInfo: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         ZStack{
             Image(.parchment)
@@ -20,29 +22,20 @@ struct ExchangeInfo: View {
                 Text("Exchange Rates")
                     .font(.largeTitle)
                     .tracking(3)
-                
+        
                 
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                 
                     .font(.title3)
                     .padding()
-                //Title text
                 
-                //description Text
-                
-                //Exchange rates
                 ExchangeRate(leftImage: .goldpiece, RightImage: .goldpiece, messagetext: "1 Gold Piece = 4 Gold Pennies")
                 ExchangeRate(leftImage: .goldpenny, RightImage: .silverpiece, messagetext: "1 Gold Penny = 4 Silver Pieces")
-                
                 ExchangeRate(leftImage: .silverpiece, RightImage: .silverpenny, messagetext: "1 Silver Piece = 4 Gold Pennies")
                 ExchangeRate(leftImage: .silverpenny, RightImage: .copperpenny, messagetext: "1 Silver Penny = 100 Copper Pennies")
-//                ExchangeRate()
-//                ExchangeRate()
-//                ExchangeRate()
-//                ExchangeRate()
                 
                 Button("Done"){
-
+                    dismiss()
                 }
                 .buttonStyle(.borderedProminent)
                 .tint(.brown.mix(with: .black, by: 0.2))
